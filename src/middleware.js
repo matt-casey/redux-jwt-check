b2a7ea65c463ec3f7edbbea35669eb5ad553d5d8
Matt Casey <matt_casey@mckinsey.com>
2016-03-28 01:07:04 -0400

    added comments

--- src/middleware.js
+++ src/middleware.js
@@ -31,24 +31,27 @@ export default function setupMiddleware({
 } = {}) {
   return ({ dispatch, getState }) => next => {
     return async action => {
+      // Doesn't need to check token
       if (!shouldCheckJwt(action, whitelistedActions, blacklistedActions)) {
         return next(action);
       }
 
-      let newToken;
       const state = getState();
       const token = getTokenFromState(state);
 
+      // Token is not expired
       if (!isTokenExpired(token)) {
         return next(action);
       }
 
+      // Cannot get new token
       if (!getNewToken) {
         return dispatch(onError());
       }
 
+      // Try to get new token
       try {
-        newToken = await getNewToken(state);
+        const newToken = await getNewToken(state);
         dispatch(onSuccess(newToken));
         return next(action);
       } catch (error) {
